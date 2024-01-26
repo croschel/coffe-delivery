4579e46d861617037ab60b7281619dc5f71b63d6
croschel <croschel000@gmail.com>
2024-01-26 13:04:28 -0300

    feat: adjust layout for info content

---
+++ src/constants/labels.ts
@@ -0,0 +1,31 @@
+export const pageLabels = {
+  home: {
+    mainTitle: 'Encontre o café perfeito para qualquer hora do dia',
+    mainDescription:
+      'Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora',
+    simpleBuy: 'Compra simples e segura',
+    intactCoffee: 'Embalagem mantém o café intacto',
+    fastDelivery: 'Entrega rápida e rastreada',
+    freshCoffee: 'O café chega fresquinho até você',
+    ourCoffees: 'Nossos cafés',
+  },
+  order: {
+    completeOrder: 'Complete seu pedido',
+    selectedCoffees: 'Cafés selecionados',
+    address: 'Endereço de Entrega',
+    addressInfo: 'Informe o endereço onde deseja receber seu pedido',
+    payment: 'Pagamento',
+    paymentInfo:
+      'O pagamento é feito na entrega. Escolha a forma que deseja pagar',
+    totalItens: 'Total de itens',
+    delivery: 'Entrega',
+    total: 'Total',
+  },
+  confirmation: {
+    orderConfirmed: 'Uhu! Pedido confirmado',
+    confirmedInfo: 'Agora é só aguardar que logo o café chegará até você',
+    orderOn: 'Entrega em',
+    orderEstimation: 'Previsão de entrega',
+    paymentOnPlace: 'Pagamento na entrega',
+  },
+};
